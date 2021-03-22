3e8c2ce269cbde871839d512747b2a51435e3f1d
Vadim Kozlenko <vadim_kozlenko@ukr.net>
2021-03-22 22:04:32 +0200

    deleted method toOverview from NetworkServices

--- src/main/java/network/services/NetworkServices.java
+++ src/main/java/network/services/NetworkServices.java
@@ -7,23 +7,6 @@ import java.util.List;
 
 public interface NetworkServices {
 
-    // This method can display model of your network like schema
-    // and display it on console
-    public static void toOverview(Node network) {
-        try {
-            System.out.println('{' + "id:" + network.getId() + '}' + " " + '{' + "name: " + network.getName() + '}' + " " + '{' + "description: " + network.getDescription() + '}');
-            for (Node node : network.getChildren()) {
-                if (node.getChildren() != null) {
-                    toOverview(node);
-                } else if (node.getChildren() == null) {
-                    System.out.println('{' + "id:" + node.getId() + '}' + " " + '{' + "name: " + node.getName() + '}' + " " + '{' + "description: " + node.getDescription() + '}');
-                }
-            }
-        } catch (NullPointerException e) {
-            System.out.println("You entered an incorrect network name, or it does not exist! ");
-        }
-    }
-
     //This method assigns a value "null" to the link to your network
     void deleteNetwork(Node network);
 
